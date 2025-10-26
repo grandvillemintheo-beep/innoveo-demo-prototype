e38c2113c57fb3d311d52ab03f278ad1d6802768
grandvillemintheo-beep <grandvillemin.theo@next-u.fr>
2025-10-27 00:31:29 +0100

    feat: remove BrowserRouter wrapper from main application entry

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,8 +2,6 @@ import './styles.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
 import { App } from './App';
 import { ThemeProvider } from './design-system/providers/ThemeProvider';
 import './i18n/config';
@@ -11,9 +9,7 @@ import './i18n/config';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
